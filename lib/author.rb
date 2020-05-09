d816da0c24849587013cc1f4b52f1ae08e38526a
Ashley Rodriguez <arodriguez1030@gmail.com>
2020-05-09 03:03:53 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,5 +1,6 @@
 class Author
   attr_accessor :name
+  @@post_count = 0
   def initialize(name)
     @name = name
     @posts= []
@@ -9,9 +10,14 @@ class Author
  end
  def add_post(post)
    post.author = self
+   @@post_count +=1
  end
  def add_post_by_title(post_title)
  post_title = Post.new(post_title)
  add_post(post_title)
+ @@post_count+=1
+ end
+ def self.post_count
+   @@post_count
  end
 end

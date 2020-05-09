016dde36979c3d3101ba2dacfb15bb465c5a1454
Ashley Rodriguez <arodriguez1030@gmail.com>
2020-05-09 02:52:04 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -10,7 +10,7 @@ class Author
  def add_post(post)
    post.author = self
  end
- def add_post_by_name(post_title)
+ def add_post_by_title(post_title)
  post_title = Post.new(post_title)
  add_post(post_title)
  end

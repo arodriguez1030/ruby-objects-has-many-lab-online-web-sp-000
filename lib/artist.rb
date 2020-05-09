57a038869b39935ed7fa1ef5c02dfd5901b54254
Ashley Rodriguez <arodriguez1030@gmail.com>
2020-05-09 03:02:40 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,6 +2,7 @@ require 'pry'
 
 class Artist
   attr_accessor :name
+  @@song_count = 0
   
   def initialize(name)
     @name = name
@@ -11,6 +12,7 @@ class Artist
  end
  def add_song(song)
    song.artist = self
+   @@song_count+=1
  end
  def add_song_by_name(song_name)
  song_name = Song.new(song_name)
